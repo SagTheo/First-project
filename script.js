f5b03434bc852669270c94c77428592732b4f8a1
SagTheo <sagniez.theo@laposte.net>
2021-10-20 20:10:00 +0200

    Worked on adding an item to the list

--- script.js
+++ script.js
@@ -5,9 +5,16 @@ const todos = document.getElementById('todos')
 btnTodo.addEventListener('click', () => {
     if (input.value != '') {
         let newTodo = input.value
-        let newItem = document.createElement('li')
+        let newItem = document.createElement('p')
         newItem.textContent = newTodo
-        todos.append(newItem)
+        let deleteItem = document.createElement('p')
+        deleteItem.className = 'deleteTodo'
+        deleteItem.textContent = 'x'
+        let todoItem = document.createElement('div')
+        todoItem.className = 'newTodo'
+        todoItem.append(newItem)
+        todoItem.append(deleteItem)
+        todos.append(todoItem)
     } else {
         alert('You must fill in the input field in order to add it to the list of to-dos.')
     }
