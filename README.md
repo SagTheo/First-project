467482618bb1ff4457572e90ffe2c9e28292f5cc
SagTheo <sagniez.theo@laposte.net>
2021-10-21 13:13:57 +0200

    Added the remove a todo functionality

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 This is my very first project
+
+
+Difficulties encountered:
+
+-adding the ability to remove an item from the list of todos:
+    -adding each new item dynamically generated to the array 'items'
+        -sorted out: had to include the for loop in the addEventListener on the button
+    -getting the todo to be removed by clicking on the cross
+        -sorted out: had to add an event listener on the cross that sets the attribute onclick to the todo
+                     (only when the pointer is on the cross), that calls a function that removes the todo when the cross is clicked 
--- script.js
+++ script.js
@@ -1,20 +1,52 @@
 const btnTodo = document.getElementById('btnTodo')
 const input = document.getElementById('todo')
 const todos = document.getElementById('todos')
+const items = document.getElementsByClassName('newTodo')
+
+const remove = (el) => {
+    let element = el
+    element.remove()
+}
+
+const onItem = (event) => {          
+    let cross = document.createElement('div')
+    cross.className = 'deleteTodo'
+    cross.textContent = 'x'
+    event.target.append(cross)
+
+    cross.addEventListener('mouseenter', () => {
+        cross.title = 'Delete the todo'
+        cross.parentNode.setAttribute('onclick', 'remove(this)')
+    })
+
+    cross.addEventListener('mouseleave', () => {
+        cross.parentNode.removeAttribute('onclick')
+    })
+    
+}
+
+const outItem = (event) => {
+    let removeCross = document.querySelector('.deleteTodo')
+    removeCross.remove()
+}
+
 
 btnTodo.addEventListener('click', () => {
     if (input.value != '') {
         let newTodo = input.value
-        let newItem = document.createElement('p')
+        input.value = ''
+        let newItem = document.createElement('div')
         newItem.textContent = newTodo
-        let deleteItem = document.createElement('p')
-        deleteItem.className = 'deleteTodo'
-        deleteItem.textContent = 'x'
         let todoItem = document.createElement('div')
         todoItem.className = 'newTodo'
         todoItem.append(newItem)
-        todoItem.append(deleteItem)
         todos.append(todoItem)
+
+        for (let item of items) {
+            item.addEventListener('mouseenter', onItem)
+            item.addEventListener('mouseleave', outItem)
+        }
+
     } else {
         alert('You must fill in the input field in order to add it to the list of to-dos.')
     }
