146509a5808b6d2690314ff82cbb3412105a9301
SagTheo <sagniez.theo@laposte.net>
2021-10-25 18:02:41 +0200

    Added the local storage feature + populating the list on refresh

--- README.md
+++ README.md
@@ -11,6 +11,12 @@ Difficulties encountered:
                      (only when the pointer is on the cross), that calls a function that removes the todo when the cross is clicked
 
 -getting the items to stay on the list after reloading the page
+    -sorted out: used localStorage
+    -making the items appear in the same order as when the user left the page (unresolved)
+    -making the items interactive (possibility to delete them) after reloading the page
+        -sorted out: had to add the for loop and the if statement in the onload function
+
 
 -making the app responsive
-    -sorted out: added media queries and played around with he width and height
+    -sorted out: added media queries and played around with the width and height
+ 
--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const input = document.getElementById('todo')
 const todos = document.getElementById('todos')
 const items = document.getElementsByClassName('newTodo')
 
+
 // Removes the element that is clicked on
 const remove = (el) => {
     let element = el
+    localStorage.removeItem(element.firstChild.textContent)
     element.remove()
     btnTodo.disabled = false
 }
@@ -29,15 +31,16 @@ const onItem = (event) => {
 }
 
 // Removes the cross when the cursor leaves the item
-const outItem = (event) => {
+const outItem = () => {
     let removeCross = document.querySelector('.deleteTodo')
     removeCross.remove()
 }
-
+ 
 // Adds a todo item to the list
 btnTodo.addEventListener('click', () => {
     if (input.value != '') {
         let newTodo = input.value
+        localStorage.setItem(newTodo, newTodo)
         input.value = ''
         let newItem = document.createElement('div')
         newItem.textContent = newTodo
@@ -54,9 +57,33 @@ btnTodo.addEventListener('click', () => {
         if (items.length === 10) {
             btnTodo.disabled = true
         } 
+ 
     } else {
         alert('You must fill in the input field in order to add it to the list of to-dos.')
     }
 })
 
-
+// Populates the list of todos with the todos that were previously added
+window.onload = () => {
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i)
+        // Filters out the localStorage own properties
+        if(localStorage.hasOwnProperty(key)) {
+            let newItem = document.createElement('div')
+            newItem.textContent = localStorage.getItem(key)
+            let todoItem = document.createElement('div')
+            todoItem.className = 'newTodo'
+            todoItem.append(newItem)
+            todos.append(todoItem)
+            
+            for (let item of items) {
+                item.addEventListener('mouseenter', onItem)
+                item.addEventListener('mouseleave', outItem)
+            }
+            
+            if (items.length === 10) {
+                btnTodo.disabled = true
+            }
+        }
+    }
+}
